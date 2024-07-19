3dd58dbdb1d768e36b772086ce4912c30145db32
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2024-07-19 12:29:24 +0200

    added webosckets' hello world

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::{get, post}, Json, Router};
 use models::Room;
+use serde_json::Value;
+use socketioxide::{extract::{Bin, Data, SocketRef}, SocketIo};
 use sqlx::PgPool;
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
 
 pub mod models;
 
@@ -11,7 +17,6 @@ async fn hello_world() -> &'static str {
 async fn add_room_handler(state: State<MyState>) -> Result<impl IntoResponse, impl IntoResponse> {
     let query = "INSERT INTO rooms DEFAULT VALUES RETURNING id, created_at";
     match sqlx::query_as::<_, Room>(query)
-        .bind("test")
         .fetch_one(&state.pool)
         .await
     {
@@ -31,6 +36,19 @@ async fn get_rooms_handler(state: State<MyState>) -> Result<impl IntoResponse, i
     }
 }
 
+fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+    info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+    socket.emit("auth", data).ok();
+
+    socket.on(
+        "message",
+        |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            socket.emit("message-back", data).ok();
+        },
+    );
+}
+
 #[derive(Clone)]
 struct MyState {
     pool: PgPool,
@@ -48,10 +66,18 @@ async fn main(
         .expect("Failed to run migrations");
     let state = MyState { pool };
 
+    let (layer, io) = SocketIo::new_layer();
+    io.ns("/", on_connect);
+
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/room", post(add_room_handler))
         .route("/rooms", get(get_rooms_handler))
+        .layer(
+            ServiceBuilder::new()
+                .layer(CorsLayer::permissive())
+                .layer(layer)
+        )
         .with_state(state);
 
     Ok(router.into())
